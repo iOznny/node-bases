b6ee72ca443d9f068507f9e59f46b09f2ad76826
iOznny <f000170014@gmail.com>
2021-05-17 20:12:07 -0500

    Tabla5

--- app.js
+++ app.js
@@ -15,5 +15,9 @@ for (let i = 1; i <=10; i++) {
 console.log(out);
 
 // Grabar archivo.
-//fs.writeFile()
+fs.writeFile(`tabla-${ base }.txt`, out, (err) => {
+    if (err) throw err;
+
+    console.log(`tabla-${ base }.txt creada.`);
+});
 
