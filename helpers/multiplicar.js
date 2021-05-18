95ed181c9dd0d32ce21f84c771537493b0278450
iOznny <f000@gm.com>
2021-05-18 16:08:49 -0500

    Avance

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -15,32 +15,29 @@ const createFile1 = (base = 5) => {
         console.log(out);
     
         // Grabar archivo.
-        /* fs.writeFile(`tabla-${ base }.txt`, out, (err) => {
+        fs.writeFile(`tabla-${ base }.txt`, out, (err) => {
             if (err) throw err;
+        });
     
-            console.log(`tabla-${ base }.txt creada.`);
-        }); */
-    
-        fs.writeFileSync(`tabla-${ base }.txt`, out);
-        console.log(`tabla-${ base }.txt creada.`);
     });
 }
 
-const createFile2 = async(base = 5) => {
+const createFile2 = async(base = 5, l = false) => {
     try {
-        let out = '';
-    
-        console.log('===============');
-        console.log('Tabla del: ', base);
-        console.log('===============');
+        let out = '';       
     
         for (let i = 1; i <=10; i++) {
             out += `${ base } x ${ i } = ${ base * i }\n`;
         }
     
-        console.log(out);
-        fs.writeFileSync(`tabla-${ base }.txt`, out);
-        console.log(`tabla-${ base }.txt creada.`);
+        if (l) {
+            console.log('===============');
+            console.log('Tabla del: ', base);
+            console.log('===============');     
+            console.log(out);
+        }
+
+        fs.writeFileSync(`tabla-${ base }.txt`, out);        
     
         return `tabla-${ base }.txt`;
     } catch (err) {
